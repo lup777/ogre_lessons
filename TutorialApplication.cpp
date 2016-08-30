9908ce5faf7d99d304684fe9570732a7251ddcac
Alexander Lapshenkov <alexander.lapshenkov@motorolasolutions.com>
2016-08-30 13:22:48 +0300

    Изначальный коммит. Пустой проект который копилится и щапускается

---
+++ TutorialApplication.cpp
@@ -0,0 +1,56 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#include "TutorialApplication.h"
+
+//-------------------------------------------------------------------------------------
+TutorialApplication::TutorialApplication(void)
+{
+}
+//-------------------------------------------------------------------------------------
+TutorialApplication::~TutorialApplication(void)
+{
+}
+
+//-------------------------------------------------------------------------------------
+void TutorialApplication::createScene(void)
+{
+    // create your scene here :)
+}
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int main(int argc, char *argv[])
+    {
+        // Create application object
+        TutorialApplication app;
+
+        try {
+            app.go();
+        } catch( Ogre::Exception& e ) {
+            std::cerr << "An exception has occured: " <<
+                e.getFullDescription().c_str() << std::endl;
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
